27646bcd50ab18cc92341ef9c8939aa479f688da
matiasresnik <mati.resnik@gmail.com>
2024-05-27 19:00:05 -0300

    Modificaciones

--- Proyecto Grupo 3 PNT1/obj/Release/net8.0/Proyecto Grupo 3 PNT1.AssemblyInfo.cs
+++ Proyecto Grupo 3 PNT1/obj/Release/net8.0/Proyecto Grupo 3 PNT1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto Grupo 3 PNT1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11f3645e771a09ccebfc471702b9d5dadea08148")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto Grupo 3 PNT1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto Grupo 3 PNT1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
